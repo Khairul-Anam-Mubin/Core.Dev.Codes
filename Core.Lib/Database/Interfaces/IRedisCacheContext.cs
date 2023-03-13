41c0a21766264f7b82fb5a29133b6eb6e17622e9
khairul_mubin <khairul.mubin@orbitax.com>
2023-03-13 16:47:54 +0600

    refactored

--- Core.Lib/Database/Interfaces/IRedisCacheContext.cs
+++ Core.Lib/Database/Interfaces/IRedisCacheContext.cs
@@ -6,12 +6,9 @@ namespace Core.Lib.Database.Interfaces
     public interface IRedisCacheContext
     {
         Task<bool> InsertItemAsync<T>(DatabaseInfo databaseInfo, T item) where T : class, IRepositoryItem;
-        Task<bool> SaveItemAsync<T>(DatabaseInfo databaseInfo, T item) where T : class, IRepositoryItem;
         Task<bool> UpdateItemAsync<T>(DatabaseInfo databaseInfo, T item) where T : class, IRepositoryItem;
         Task<bool> DeleteItemByIdAsync<T>(DatabaseInfo databaseInfo, string id) where T : class, IRepositoryItem;
-        Task<bool> DeleteItemsByFilterDefinitionAsync<T>(DatabaseInfo databaseInfo, FilterDefinition<T> filterDefinition) where T : class, IRepositoryItem;
         Task<T> GetItemByIdAsync<T>(DatabaseInfo databaseInfo, string id) where T : class, IRepositoryItem;
         Task<List<T>> GetItemsAsync<T>(DatabaseInfo databaseInfo) where T : class, IRepositoryItem;
-        Task<T> GetItemByFilterDefinitionAsync<T>(DatabaseInfo databaseInfo, FilterDefinition<T> filterDefinition) where T : class, IRepositoryItem;
     }
 }
